c57b3d5cb2090eb050da68c53f8864ff0fcfe611
hoangnguyen2108 <langtu092000@gmail.com>
2020-10-29 19:07:17 -0400

    update for herou

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+ // "URI": "mongodb://localhost/books229"
 
- // "URI": "mongodb+srv://langtu092000:longbadao@hoang.sba8w.mongodb.net/langtu092000?retryWrites=true&w=majority"
+  "URI": "mongodb+srv://langtu092000:longbadao@hoang.sba8w.mongodb.net/langtu092000?retryWrites=true&w=majority"
 };
